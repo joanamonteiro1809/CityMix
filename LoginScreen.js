250e168e204f0a5cfb57430227568fa644f23979
Beatriz Rosas <b.rosas@campus.fct.unl.pt>
2024-11-16 22:08:09 +0000

    Saves new tours added.

--- LoginScreen.js
+++ LoginScreen.js
@@ -31,6 +31,8 @@ const LoginScreen = ({ navigation }) => {
 
                 {/* Login Title */}
                 <Text style={styles.title}>Login</Text>
+                <Text style={{ fontFamily: 'CodecPro-Regular' }}>CodecPro-Regular</Text>
+                <Text style={{ fontFamily: 'CodecPro-Italic' }}>CodecPro-Italic</Text>
 
                 {/* Email and Password Input */}
                 <TextInput style={styles.input} placeholder="Email" placeholderTextColor="#888" />
